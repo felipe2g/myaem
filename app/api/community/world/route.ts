a867f0c30624a1d741a654e2869489d1c82e182a
Felipe Guimarães <felipe2g.dev@gmail.com>
2024-05-30 15:25:52 -0300

    add world route

--- app/api/community/world/route.ts
+++ app/api/community/world/route.ts
@@ -1,26 +1,6 @@
 import prisma from "@/lib/db";
 import { NextRequest } from "next/server";
 
-// TODO: Filters
-// World:	
-// All Worlds
-// BattleEye:	
-// Any World
-// Vocation:	
-// (all)
-// Category:	
-// Experience Points
-// World Type:	
-// Open PvP
-// Optional PvP
-// Hardcore PvP
-// Retro Open PvP
-// Retro Hardcore PvP
-
-(BigInt.prototype as any).toJSON = function () {
-  return this.toString();
-};
-
 const DEFAULT_PAGE = 1;
 const DEFAULT_LIMIT_PER_PAGE = 50;
 const MAX_LIMIT_PER_PAGE = 100;
@@ -30,25 +10,28 @@ export async function GET(req: NextRequest) {
 
   const pageParam = Number(searchParams.get('page'));
   const page = pageParam > 0 ? pageParam : DEFAULT_PAGE;
-  
+
   const limitParam = Number(searchParams.get('limit'));
   const limit = limitParam > 0 ? limitParam : DEFAULT_LIMIT_PER_PAGE;
-  
+
   const take = Math.min(Number(limit) || DEFAULT_LIMIT_PER_PAGE, MAX_LIMIT_PER_PAGE);
 
   const skip = (page - 1) * take;
 
   try {
-    const players = await prisma.players.findMany({
-      select: {
-        name: true,
-        level: true,
-        vocation: true,
-        experience: true,
+    const players = await prisma.playerOnline.findMany({
+      include: {
+        player: {
+          select: {
+            name: true,
+            level: true,
+            vocation: true,
+          }
+        }
       },
       take,
       skip,
-    });
+    })
 
     return Response.json(players);
   } catch (error) {
